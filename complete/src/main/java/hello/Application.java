28cdad85343d730a0e0fc32d05bfcd86c5119be2
Binu Chennattu Zachariah <78688174+binuzac@users.noreply.github.com>
2021-09-25 11:10:09 +0400

    Update Application.java
    Test update

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -12,6 +12,7 @@ public class Application {
 	@RequestMapping("/")
 	public String home() {
 		return "Welcome to my Devops Capstone Project for Aetna Healthcare";
+		return "Welcome to my Devops Capstone Project for Aetna Healthcare";
 	}
 
 	public static void main(String[] args) {
