80ff97a722cd19aa5e32bb89f79c46b3e3f9c28c
Binu Chennattu Zachariah <78688174+binuzac@users.noreply.github.com>
2021-09-26 19:56:29 +0400

    UpdateApplication7

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Welcome to Aetna Docker World Update 6";
+		return "Welcome to Aetna Docker World Update 7";
 	}
 
 	public static void main(String[] args) {
